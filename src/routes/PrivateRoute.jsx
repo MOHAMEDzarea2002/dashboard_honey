fe7c59158524093b9a1ea3cd810be1784556a86b
mohamed zarea <mohamedzarea@gmial.com>
2026-08-25 15:22:29 +0300

    fix: project  Loading

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -4,7 +4,7 @@ export default function PrivateRoute() {
   const { isAuthenticated, authLoading } = useSelector((state) => state.auth);
 
   if (authLoading) {
-    return <div>loading</div>;
+    return <div className=" flex justify-center items-center bg-black h-screen text-4xl animate-pulse text-white">loading..</div>;
   }
 
     return isAuthenticated ? <Outlet/> : <Navigate to="/login" replace />
